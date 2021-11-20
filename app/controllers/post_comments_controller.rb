6bd76917dc2ee7a8d0681bc47f5790ca7887cca7
Kirill Khalevin <kirill.khalevin.hexlet@gmail.com>
2021-11-20 22:41:33 +0300

    rubocop ammend

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,11 +3,11 @@ class PostCommentsController < ApplicationController
     post_ids = params.permit(:post_id)
     ancestry = params.permit(:ancestry)
     post_comment_params = params.require(:post_comment).permit(:content)
-    if ancestry[:ancestry].present?
-      obj = PostComment.find(ancestry[:ancestry]).children
-    else
-      obj = PostComment
-    end
+    obj = if ancestry[:ancestry].present?
+            PostComment.find(ancestry[:ancestry]).children
+          else
+            PostComment
+          end
     obj.create(post_comment_params.merge(post_ids).merge({ user_id: current_user.id })).persisted?
   end
 end
