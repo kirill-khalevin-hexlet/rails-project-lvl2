da352c1e67be7bf3b21402f1376b76f64d1db6a2
Kirill Khalevin <kirill.khalevin.hexlet@gmail.com>
2021-11-20 22:33:31 +0300

    Ancestry create messages in tree

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,7 +1,13 @@
 class PostCommentsController < ApplicationController
   def create
-    post_id = params.permit(:post_id)
+    post_ids = params.permit(:post_id)
+    ancestry = params.permit(:ancestry)
     post_comment_params = params.require(:post_comment).permit(:content)
-    PostComment.create(post_comment_params.merge(post_id).merge({ user_id: current_user.id })).persisted?
+    if ancestry[:ancestry].present?
+      obj = PostComment.find(ancestry[:ancestry]).children
+    else
+      obj = PostComment
+    end
+    obj.create(post_comment_params.merge(post_ids).merge({ user_id: current_user.id })).persisted?
   end
 end
